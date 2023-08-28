1671d8451c287d45b0c309aa343ca11e73e42d4c
jegomezV <jegomezvalencia@gmail.com>
2023-08-28 14:13:10 -0500

    8_make

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,12 +2,15 @@
 """
 |- - - - - - - - - - - - - - - - - - - - - - - - - - |
 | Module:                                            |
-|6-sum_mixed_list.py                                 |
+|7-to_kv.py                                          |
 |                                                    |
 |Description:                                        |
-|Write a type-annotated function sum_mixed_list      |
-|which takes a list mxd_lst of integers and floats   |
-|and returns their sum as a float.                   |
+|Write a type-annotated function to_kv               |
+|that takes a string k and an int OR float           |
+|v as arguments and returns a tuple.                 |
+|The first element of the tuple is the string k.     |
+|The second element is the square of the int/float v |
+|and should be annotated as a float.                 |
 |- - - - - - - - - - - - - - - - - - - - - - - - - - |
 """
 from typing import Tuple, Union
---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| Module:                                                      |
+|8-make_multiplier.py                                          |
+|                                                              |
+|Description:                                                  |
+|Write a type-annotated function make_multiplier               |
+|that takes a float multiplier as argument                     |
+|and returns a function that multiplies a float by multiplier. |
+|- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    return lambda x: x * multiplier
