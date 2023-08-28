cc4140576502f2b92db6f4dbf557661fe5b1cf50
jegomezV <jegomezvalencia@gmail.com>
2023-08-28 10:32:43 -0500

    1-concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+--------------------------------------------------|
+| Module:                                         |
+|1-concat.py                                      |
+|                                                 |
+|Description:                                     |
+|Write a type-annotated function                  |
+|concat that takes a string str1 and a string str2|
+|as arguments and returns a concatenated string.  |
+|-------------------------------------------------|
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat funct"""
+    return str1 + str2
