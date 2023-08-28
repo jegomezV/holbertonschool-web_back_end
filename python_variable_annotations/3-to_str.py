86ebf00bd5a3982b307581b63ef944d02b1b7cec
jegomezV <jegomezvalencia@gmail.com>
2023-08-28 11:33:45 -0500

    3-to_str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+"""
+|- - - - - - - - - - - - - - - - - - - - - - - - - - |
+| Module:                                            |
+|3-to_str.py                                         |
+|                                                    |
+|Description:                                        |
+|Write a type-annotated function to_str              |
+|that takes a float n as argument                    |
+|and returns the string representation of the float. |
+|- - - - - - - - - - - - - - - - - - - - - - - - - - |
+"""
+
+
+def to_str(n: float) -> str:
+    return str(n)
