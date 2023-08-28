f46f41107eabfadaba904f2c2a0bb6e0ec368217
jegomezV <jegomezvalencia@gmail.com>
2023-08-28 13:23:21 -0500

    7-to_kv

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+|- - - - - - - - - - - - - - - - - - - - - - - - - - |
+| Module:                                            |
+|6-sum_mixed_list.py                                 |
+|                                                    |
+|Description:                                        |
+|Write a type-annotated function sum_mixed_list      |
+|which takes a list mxd_lst of integers and floats   |
+|and returns their sum as a float.                   |
+|- - - - - - - - - - - - - - - - - - - - - - - - - - |
+"""
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) ->  Tuple[str, float]:
+    return (k, v*v)
