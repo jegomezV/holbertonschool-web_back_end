729efb16b3ea49f3253e1bc1a44355634304c53d
jegomezV <jegomezvalencia@gmail.com>
2023-09-08 03:36:23 -0500

    2-Question

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+"""
+Write a function named index_range
+that takes two integer arguments page and page_size.
+
+The function should return a tuple of size two
+containing a start index and an end index corresponding
+to the range of indexes to return in a list
+for those particular pagination parameters.
+
+Page numbers are 1-indexed, i.e. the first page is page 1.
+"""
+from typing import Tuple, List
+import csv
+import math
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        method named get_page that takes two integer arguments page with
+        default value 1 and page_size with default value 10.
+        """
+        assert isinstance(page, int) and \
+            isinstance(page_size, int) and page > 0 and page_size > 0
+        index: tuple = index_range(page, page_size)
+
+        return self.dataset()[index[0]: index[1]]
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    The function should return a tuple of size two containing
+    a start index and an end index corresponding to the range of indexes
+    to return in a list for those particular pagination parameters.
+    """
+    return ((page - 1) * page_size, page * page_size)
+
+
+def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+    """return a dictionary containing the following key-value pairs """
+    try:
+        if len(self.get_page(page + 1, page_size)) < page_size:
+            next_page = None
+        else:
+            next_page = page + 1
+    except Exception:
+        next_page = None
+
+    try:
+        self.get_page(page - 1, page_size)
+        prev_page = page - 1
+    except Exception:
+        prev_page = None
+
+    dict = {
+            "page_size": page_size,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": int(len(self.dataset()) / page_size),
+    }
+    return dict
