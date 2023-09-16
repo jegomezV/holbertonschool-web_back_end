cfd2249a7c30e31b7f2d30a994afcf02cb967bd2
jegomezV <jegomezvalencia@gmail.com>
2023-09-15 21:35:39 -0500

    0_Question

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    parser: 'babel-eslint',
+    env: {
+      node: true,
+      es6: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 2018,
+    },
+    rules: {
+      // Aquí puedes agregar reglas personalizadas si lo deseas
+    },
+  };
+  
